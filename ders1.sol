54ca2500470ad806e236bcf2201dcc06b369d00d
berk cicek <90208101+berkcicekk@users.noreply.github.com>
2023-03-31 00:32:56 +0300

    Update ders1.sol

--- ders1.sol
+++ ders1.sol
@@ -1,5 +1,9 @@
 pragma solidity 0.8.0; //solidity versiyonu 0.8.0 olduğunu belirtiyor.
 
+/**
+ * @author berk cicek
+ */
+ 
 contract HelloWorld { //contract ismi HelloWorld
 
     function PrintHelloWorld() public returns(string memory) { //fonksiyon ismi PrintHelloWorld, fonksiyonun döndürdüğü değer string memory, fonksiyonun erişim tipi public
